fa078cee15e2a99d5f40f57de29befab52111780
Edward Harley <edharley19@gmail.com>
2019-11-07 18:38:27 -0500

    css issues fixed

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,1 +1,19 @@
 var words =  ["extraterrestrial", "stellar", "galaxy", "planet", "asteroid"];
+function chooseWord() {
+    compWord = words[Math.floor(Math.random() * words.length)];
+    console.log(compWord);
+    
+}
+
+chooseWord();
+var letterSpaces = [];
+for (var i = 0; i < compWord.length; i++) { 
+    letterSpaces[i] = "_";
+   
+}
+
+document.getElementById("answerSpace").
+
+
+
+
