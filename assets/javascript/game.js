5da1068cabd5e3003f6ddd71f98bc33fae2d6c1f
Edward Harley <edharley19@gmail.com>
2019-11-09 09:52:15 -0500

    variables added, fixed loading issue

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,18 +1,44 @@
-var words =  ["extraterrestrial", "stellar", "galaxy", "planet", "asteroid"];
-function chooseWord() {
-    compWord = words[Math.floor(Math.random() * words.length)];
-    console.log(compWord);
+
+
+
+
+window.onload = () => {
+
+    var words =  ["extraterrestrial", "stellar", "galaxy", "planet", "asteroid"];
+    function chooseWord() {
+        return words[Math.floor(Math.random() * words.length)];
+    }
     
-}
+    
+    var compWord = chooseWord();
+    
+    var letterSpaces = "";
+    for (var i = 0; i < compWord.length; i++) { 
+        letterSpaces += "_";
+    }
 
-chooseWord();
-var letterSpaces = [];
-for (var i = 0; i < compWord.length; i++) { 
-    letterSpaces[i] = "_";
-   
-}
+    document.getElementById("answerSpace").innerHTML = `<h4>${letterSpaces}</h4>`;
+    document.addEventListener('keypress', function(event){
+        console.log(event);
+    });
+    
 
-document.getElementById("answerSpace").
+    // console.log("Hello")
+    // document.getElementById("answerSpace").innerHTML = "HELLO";
+};
+
+
+// create scoreboard to keep track of user correctly guessed words
+var score = 0;
+
+// give user limited number of chances
+var chances = 3;
+// have user guess letters of the word 
+
+// show correct letters in spaces 
+// show incorrect letters in separate section
+// if user guesses all the letters in the compWord, give user a point
+// if user cannot guess all the letters in the compWord before chances run out, end game
 
 
 
